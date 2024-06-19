4a2f02b17b3e5fa6106df86077ae29e57cc7035d
Emads <emadsaab722@gmail.com>
2024-06-19 20:30:03 +0300

    refactor files

--- config.py
+++ config.py
@@ -2,9 +2,7 @@ from pathlib import Path
 
 ASSETS = Path(__file__).parent / "assets"
 
-LOGS = ASSETS / "log"
 RESOURCES = ASSETS / "resources"
-LOG_FILE = LOGS / "app.log"
 DATA_FILE = RESOURCES / "data.csv"
 
 # Define the API routes
--- main.py
+++ main.py
@@ -1,17 +1,5 @@
-import logging
 import justpy as jp
 from wordwiz_api import WordWizAPI
-from config import LOG_FILE
-
-
-# Create the directory for log files if it doesn't exist, and Ensure parent directories are created if they don't exist
-LOG_FILE.parent.mkdir(parents=True, exist_ok=True)
-
-
-# Configure the logging format and level
-logging.basicConfig(filename=LOG_FILE,
-                    format='%(asctime)s - %(levelname)s - %(message)s (%(module)s:%(filename)s:%(lineno)d)',
-                    level=logging.DEBUG)
 
 
 # Register the API route with JustPy
